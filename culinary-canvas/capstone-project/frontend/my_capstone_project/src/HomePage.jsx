7c557292be396ceec6fcf56f1a4af7105a04760a
Dara Oyedun <oluwadara@meta.com>
2024-06-28 10:52:58 -0700

    log-in-modification

--- culinary-canvas/capstone-project/frontend/my_capstone_project/src/HomePage.jsx
+++ culinary-canvas/capstone-project/frontend/my_capstone_project/src/HomePage.jsx
@@ -1,5 +1,5 @@
 import './HomePage.css'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './Header';
 import Banner from './Banner';
 import { useNavigate } from "react-router-dom";
@@ -7,7 +7,8 @@ import { useAuth } from './RedirectToAuthentication'
 function HomePage(){
     const navigate = useNavigate();
     const { isAuthenticated, user, logOut} = useAuth();
-    
+    const [recipes, setRecipes] = useState([])
+
     const LogOut = async() =>{
         try{
             await logOut();
@@ -17,7 +18,21 @@ function HomePage(){
             console.log(error)
         }
     }
-    console.log("HomePage Render", { isAuthenticated, user })
+
+    useEffect(() => {
+        const fetchRecipies = async() => {
+            try{
+                const response = await fetch(`https://api.edamam.com/search?q=beans&app_id=${import.meta.env.VITE_EDAMAM_APP_ID}&app_key=${import.meta.env.VITE_EDAMAM_APP_KEY}`)
+                if(!response.ok){
+                    throw new Error("Error fetching recipes")
+                }
+                const data = await response.json();
+                setRecipes(data.hits.map(hit => hit.recipe));
+            }catch(error){
+                console.error('Error fetching recipies: ', error)
+            }
+        }; fetchRecipies();
+    }, [])
     return (
         <>
             <Header/>
@@ -35,6 +50,17 @@ function HomePage(){
                 </>
             )}
             <Banner/>
+            <h1>Beans</h1>
+            <div className='recipes'>
+                {recipes.map((recipe, index) => (
+                    <div key={index} className='recipe'>
+                        <h3>{recipe.label}</h3>
+                        <img src={recipe.image} alt={recipe.label}/>
+                        <p>{recipe.ingredientLines.join(' , ')}</p>
+                    </div>
+                ))}
+            </div>
+            
         </>
     )
 }
