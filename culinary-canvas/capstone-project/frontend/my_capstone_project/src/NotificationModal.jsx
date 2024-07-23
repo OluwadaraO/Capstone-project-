23292d43c0618f624871e0dba857b393d97b99a2
Dara Oyedun <oluwadara@meta.com>
2024-07-23 10:45:22 -0700

    Users who have subscribed to push notifications no longer see the Notification modal

--- culinary-canvas/capstone-project/frontend/my_capstone_project/src/NotificationModal.jsx
+++ culinary-canvas/capstone-project/frontend/my_capstone_project/src/NotificationModal.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import { useNotifications } from './useNotification';
-function NotificationModal({onClose, userId}) {
-    const {isSubscribed, subscribeToNotifications} = useNotifications(userId);
+import React, { useEffect } from "react";
+import { useNotifications } from "./useNotification";
+function NotificationModal({ onClose, userId }) {
+  const { isSubscribed, subscribeToNotifications } = useNotifications(userId);
 
-    const handleSubscribe = async() => {
-        try{
-            await subscribeToNotifications();
-            onClose();
-        }catch(error){
-            console.error("Failed to subscribe user")
-        }
-    };
-    return(
-        <div className='modal-overlay'>
-            <div className='modal-content'>
-                <h3>Stay Updated With Culinary Canvas</h3>
-                <p>Enable notifications to get the lastest dietary tips!</p>
-                <button onClick={handleSubscribe} disabled={isSubscribed}>
-                    {isSubscribed ? 'Notifications Enabled' : 'Enable Notifications'}
-                </button>
-                <button onClick={onClose}>Maybe Later</button>
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    if (isSubscribed) {
+      onClose();
+    }
+  }, [isSubscribed, onClose]);
+
+  if (isSubscribed) {
+    return null;
+  }
+  const handleSubscribe = async () => {
+    try {
+      await subscribeToNotifications();
+      onClose();
+    } catch (error) {
+      console.error("Failed to subscribe user");
+    }
+  };
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>
+          &times;
+        </button>
+        <h3>Stay Updated With Culinary Canvas</h3>
+        <p>Enable notifications to get the lastest dietary tips!</p>
+        <button onClick={handleSubscribe}>
+          {isSubscribed ? "Notifications Enabled" : "Enable Notifications"}
+        </button>
+        <button onClick={onClose}>Maybe Later</button>
+      </div>
+    </div>
+  );
 }
-export default NotificationModal
+export default NotificationModal;
--- culinary-canvas/capstone-project/frontend/my_capstone_project/src/socket.js
+++ culinary-canvas/capstone-project/frontend/my_capstone_project/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 const backendAddress = import.meta.env.VITE_BACKEND_ADDRESS;
 let socket;
 export const getSocket = () => {
-    if (!socket){
+    if (!socket) {
         socket = io(backendAddress, {
             withCredentials: true,
         });
